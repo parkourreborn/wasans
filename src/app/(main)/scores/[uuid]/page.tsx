ea6f25a20ce877caf12857da02f604d128518029
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-04-25 17:03:11 -0500

    Add score view layout

--- src/app/(main)/scores/[uuid]/page.tsx
+++ src/app/(main)/scores/[uuid]/page.tsx
@@ -1,32 +1,40 @@
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+"use client";
+
+import Badges from "@/components/custom/badges";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { CopyIcon } from "lucide-react";
+import React from "react";
 
 
-export default async function Home({ params }: Readonly<{ params: { uuid: string } }>) {
-  const {uuid} = await params;
+export default function Home({ params }: Readonly<{ params: Promise<{ uuid: string }> }>) {
+  const paramsData = React.use(params);
+  const uuid = paramsData.uuid;
   return (
     <div className="w-full h-full flex items-center justify-center">
       <Card>
         <CardHeader>
-          <div className="w-full flex flex-col">
-            <div className="w-fit flex flex-row items-center justify-between gap-4">
-              <h2 className="text-3xl font-semibold">Genesis 8.318</h2>
-              <Badge variant="fwr">🏆 WR</Badge>
-              <Badge variant="approved">Approved</Badge>
-              <Badge variant="outline">Pending</Badge>
-              <Badge variant="denied">Denied</Badge>
+          <div className="w-full flex flex-col gap-2">
+            <div className="w-full flex items-center justify-between">
+              <div className="w-full flex items-center justify-start gap-4">
+                <h2 className="lg:text-3xl font-bold">Genesis 8.318</h2>
+                <Separator orientation="vertical"/>
+                <p className="lg:text-lg text-muted-foreground">Tel Aviv</p>
+                <Separator orientation="vertical"/>
+                <p className="text-muted-foreground">04/21/2026</p>
+              </div>
+              <Button className="cursor-pointer" variant="ghost" onClick={() => navigator.clipboard.writeText(`https://wasans.tully.sh/scores/${uuid}`)}>
+                <CopyIcon />
+              </Button>
             </div>
-            <p className="text-lg text-muted-foreground">Tel Aviv</p>
+
+            <Badges badges={["wr", "approved"]} />
           </div>
         </CardHeader>
         <CardContent>
           <video controls src={`https://assets.wasans.tully.sh/scores/${uuid}.mp4`} />
         </CardContent>
-        {/* <CardFooter>
-          <div className="w-full flex items-center justify-between">
-            <Badge variant="destructive">DDT</Badge>
-          </div>
-        </CardFooter> */}
       </Card>
     </div>
   );
