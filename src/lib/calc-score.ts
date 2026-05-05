535b06ba9014ec69b8b858bb567a617c8e1a90d0
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-05-05 10:15:25 -0500

    Implement new scoring method

--- src/lib/calc-score.ts
+++ src/lib/calc-score.ts
@@ -1,7 +1,24 @@
 import { plats, TrialName } from "./trials";
 
 export default function calculateScore(wr: number, time: number, trial: TrialName) {
-    return Math.min(
-        Math.pow(wr / time, 3),
-    1)
+    if (Math.pow((wr / time), 3) > 1) {
+        return 1;
+    }
+
+    if (time > plats[trial]) {
+        return 0.3 * (plats[trial] / time);
+    }
+
+    return (
+        0.3 + 0.7 *
+        ((Math.pow((wr / time), 3) - Math.pow((wr / plats[trial]), 3)) /
+            (1 - Math.pow((wr / plats[trial]), 3)))
+    )
 }
+
+/*
+=iferror(IF((wr/time)^3>1,"no", <-- prevent time input being lower than wr
+IF(time>platinum, <-- check if slower than platinum
+0.3*(platinum/time), <-- 0.3 maximum non-exponential calculation for times below plat
+0.3 + 0.7*(((wr/time)^3 - (wr/platinum)^3) / (1-(wr/platinum )^3)))), 0) <-- dumbshit ohhhhhhhhhhhhhhhhhhh
+*/
