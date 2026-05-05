19ac1b061f825188b4410448fad34cf850578d9e
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-05-04 23:36:29 -0500

    Return to intial algorithm

--- src/lib/calc-score.ts
+++ src/lib/calc-score.ts
@@ -1,15 +1,7 @@
 import { plats, TrialName } from "./trials";
 
 export default function calculateScore(wr: number, time: number, trial: TrialName) {
-    if (time <= plats[trial]) {
-        return Math.max(
-            Math.min(
-                wr / plats[trial] +  (1 - wr / plats[trial] ) * Math.pow(wr / time, 3),
-            1),
-        0.3);
-    }
-
     return Math.min(
-        0.3 * (plats[trial] / time),
-    0.3);
+        Math.pow(wr / time, 3),
+    1)
 }
